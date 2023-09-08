8b4051842c3d95051baaa02ae0272cac9e6fa99d
0huey <34045167+0huey@users.noreply.github.com>
2023-09-07 19:19:16 -0600

    remove old code

--- 03-kernel/kernel/main.c
+++
@@ -1,9 +0,0 @@
-void main (void) {
-	char* video_mem = (char*)0xb8000;
-
-	const char* msg = "running da kernel";
-
-	for (; *msg != '\0'; msg++, video_mem += 2) {
-		*video_mem = *msg;
-	}
-}
--- 04-VGA/kernel/kernel-entry.asm
+++
@@ -1,8 +0,0 @@
-BITS 32
-extern main
-global _start
-
-_start:
-	call main
-	cli
-	hlt
